fc8ff91c5be696817ca8efec3477e01ad2b56b19
coal-rock <colebrazinski15@gmail.com>
2025-02-25 09:10:13 -0500

    package.json should be fixed now

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
+import { HomePage } from './pages/Home.page';
 
 export default function App() {
   return (
     <MantineProvider theme={theme} defaultColorScheme='dark'>
-      <Router />
+      <HomePage/>
     </MantineProvider>
   );
 }
