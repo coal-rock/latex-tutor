2b94c02dd628088f545d9b468717da509a4e1b8f
coal-rock <colebrazinski15@gmail.com>
2025-02-25 19:16:52 -0500

    pls

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: './',
   test: {
     globals: true,
     environment: 'jsdom',
